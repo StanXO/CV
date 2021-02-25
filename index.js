c98a34388eab1f3f522567c5263d2a2b6abe12d8
StanXO <77334826+StanXO@users.noreply.github.com>
2021-02-25 10:14:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 var yourName=prompt("Please enter your name: ")
 document.getElementById("welcome").innerHTML="Hello "+yourName;
+document.getElementById("pika").innerHTML="Wanna collaborate "+yourName+" ?";
